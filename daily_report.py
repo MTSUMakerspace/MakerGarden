d2f11e2a3fad25e183732d18357dcbf29d8787e2
mis2n <mis2n@mtmail.mtsu.edu>
2023-04-05 12:55:42 -0500

    re-uploading due to merge issue

---
+++ daily_report.py
@@ -0,0 +1,43 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+### READ DATA LOG AND PRODUCE PLOTS
+with open("/home/pi/MakerGarden/sensor.log", 'r') as f:
+	text = f.readlines()
+	f.close()
+x = []
+y = []
+for i in range(len(text)):
+	data = text[i].split("***")
+	dt = data[0] + " " + data[1]
+	sm = int(data[-1].split()[-1].strip()[:-5])
+	x.append(dt)
+	y.append(sm)
+
+
+# Create and save plot for last 3 days
+fig1, ax1 = plt.subplots(1)
+fig1.autofmt_xdate()
+plt.title("Maker Garden 3-Day Moisture Log")
+plt.xlabel("Date & Time")
+plt.ylabel("Moisture Reading")
+plt.plot(x[-36:], y[-36:])
+plt.savefig("/home/pi/MakerGarden/plot1.png")
+
+# Create and save all-time data plot
+fig1, ax1 = plt.subplots(1)
+fig1.autofmt_xdate()
+plt.title("Maker Garden Moisture Log")
+plt.xlabel("Date & Time")
+plt.ylabel("Moisture Reading")
+plt.plot(x, y)
+plt.savefig("/home/pi/MakerGarden/plot2.png")
+
+cmnd = "sudo python3 /home/pi/MakerGarden/notify.py"
+#os.system(cmnd)
+
+
+
+
+
---
+++ setup_mkrgrdn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Be sure to do sudo apt update and sudo apt install before running script
+
+sudo pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo python3 raspi-blinka.py
+cp /home/pi/MakerGarden/ipconfifiles/dhcpcd.conf /etc/
+cp /home/pi/MakerGarden/ipconfifiles/hostname /etc/
+cp /home/pi/MakerGarden/ipconfifiles/hosts /etc/
+sudo systemctl enable ssh
+sudo systemctl start ssh
+sudo reboot
