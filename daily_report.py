bf42e8ac32d5ec6b7b98c71bc3662453eba8c145
Matthew I. Swindall <57153699+mis2n@users.noreply.github.com>
2023-04-20 11:05:29 -0500

    Fixing legibility of plots

--- daily_report.py
+++ daily_report.py
@@ -18,24 +18,23 @@ for i in range(len(text)):
 
 # Create and save plot for last 3 days
 fig1, ax1 = plt.subplots(1)
-fig1.autofmt_xdate()
-plt.title("Maker Garden 3-Day Moisture Log")
-plt.xlabel("Date & Time")
+#fig1.autofmt_xdate(rotation=90)
+ax1.set_xticks([23, 47, 71, 95, 119, 143, 167])
+ax1.set_xticklabels(['1', '2', '3' ,'4', '5', '6', '7'])
+plt.title("Maker Garden 7-Day Moisture Log")
+plt.xlabel("Days")
 plt.ylabel("Moisture Reading")
-plt.plot(x[-36:], y[-36:])
+plt.plot(x[-168:], y[-168:])
 plt.savefig("/home/pi/MakerGarden/plot1.png")
 
 # Create and save all-time data plot
 fig1, ax1 = plt.subplots(1)
-fig1.autofmt_xdate()
+plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 plt.title("Maker Garden Moisture Log (all data)")
-plt.xlabel("Date & Time")
+plt.xlabel("Date Range: " + x[0] + " - " + x[-1])
 plt.ylabel("Moisture Reading")
 plt.plot(x, y)
 plt.savefig("/home/pi/MakerGarden/plot2.png")
-
-cmnd = "sudo python3 /home/pi/MakerGarden/notify.py"
-#os.system(cmnd)
 
 
 
