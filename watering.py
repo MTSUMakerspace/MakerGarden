a6a6a43565efa486f315f7cb7ee119c7344290a3
mis2n <mis2n@mtmail.mtsu.edu>
2023-11-28 14:10:54 -0600

    correcting watering threshold

--- watering.py
+++ watering.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-threshold = 600
+threshold = 650
 cmnd = "python3 /home/pi/MakerGarden/sprinkle.py"
 
 with open("/home/pi/MakerGarden/sensor.log", 'r') as f:
